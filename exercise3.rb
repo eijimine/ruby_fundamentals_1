d7acaa3d9a58d60c3cb3c9837d7b6ceba52435e7
eiji <eijimine@gmail.com>
2017-06-26 14:26:40 -0400

    Starting exercise 4

--- exercise3.rb
+++ exercise3.rb
@@ -4,3 +4,10 @@ puts "Hello, #{user_name}."
 puts "How old are you?"
 user_age = gets.chomp.to_i
 puts "#{user_name}, you were born in #{2017 - user_age}."
+
+counter = 1
+
+while counter < 4
+  puts "Counter is currently at #{counter}."
+  counter += 1
+end
