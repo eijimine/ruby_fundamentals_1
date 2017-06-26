1c551aad67ca72e6610c5111cb105a12c4babbab
eiji <eijimine@gmail.com>
2017-06-26 13:55:58 -0400

    Variables and assignment

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+# Calculating 15% tip
+meal = 55
+tip = 0.15
+total = meal * tip + meal
+puts "Meal including tip is #{total}!"
+
+
+# Converting number to string
+mynumber = "My number is " + 12345.to_s
+puts "#{mynumber}"
+
+answer = 45628 * 7839
+puts "The answer to question 3 is #{answer}!"
+
+boolean = (10 < 20 && 30 < 20) || !(10 == 11)
+puts "#{boolean}"
