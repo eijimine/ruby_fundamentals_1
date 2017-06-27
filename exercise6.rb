69974dafd0df441a1422ca4063bceebacdf6d280
eiji <eijimine@gmail.com>
2017-06-26 21:40:48 -0400

    All Complete

--- exercise6.rb
+++ exercise6.rb
@@ -1,19 +1,36 @@
 distance = 0
-energy = 10
+energy = 3
 puts "Press any key to start. When finished, enter \"go home\"."
 input = gets.chomp
+
 while input != "go home"
   puts "Would you like to walk or run?"
   input = gets.chomp
   if input == "walk"
     distance += 1
-    energy += 3
+    energy += 1
     puts "Distance from home is #{distance}km."
-    puts "Your energy level is #{energy}."
-  else input == "run"
+    puts "Your energy is #{energy}"
+  elsif input == "run"
+    if energy <= 0
+      puts "You are out of energy"
+      puts "Would you like to rest? \"yes\" or \"no\"."
+      enter = gets.chomp
+      if enter == "yes"
+        energy += 3
+        puts"Your energy is now #{energy}."
+      else
+        puts "thats sad :( !!!"
+      end
+
+    else
     distance += 5
-    energy -= 5
     puts "Distance from home is #{distance}km."
-    puts "Your energy level is #{energy}."
+    puts "Your energy is #{energy}"
+    energy -= 1
   end
-end
+elsif input == "go home"
+    puts"Bye !! tc! :)"
+  end
+
+  end
